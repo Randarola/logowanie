a205a42a1cf8fc549887edaa743db71651f715a4
Randarola <67556224+Randarola@users.noreply.github.com>
2021-12-06 10:11:39 +0100

    removing unnecessary elements of the original code

--- src/pl/rou/programmingLanguages/MainWindow.java
+++ src/pl/rou/programmingLanguages/MainWindow.java
@@ -13,7 +13,7 @@ public class MainWindow extends JFrame {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 try {
-                    MainWindow window = new MainWindow("logowanie");
+                    MainWindow window = new MainWindow("uciekajacy przycisk");
                     window.setVisible(true);
                 } catch (Exception e) {
                     e.printStackTrace(System.err);
@@ -23,10 +23,10 @@ public class MainWindow extends JFrame {
     }
     public MainWindow(String title) throws Exception {
         super(title);
-        UsersData usersData = new UsersData();
-        buildFrame(usersData.getLoginInfo());
+
+        buildFrame();
     }
-    protected void buildFrame(HashMap <String, String> loginInfo){
+    protected void buildFrame(){
 
         setBounds(100, 100, 450, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -34,34 +34,19 @@ public class MainWindow extends JFrame {
         JButton login = new JButton("Log in");
         JButton cancel = new JButton("Cancel");
 
-        JTextField userNameField = new JTextField();
-        JPasswordField userPasswordField = new JPasswordField();
 
         JLabel userLoginLabel = new JLabel("Login: ");
-        JLabel userPasswordLabel = new JLabel("Password: ");
-        JLabel messageLabel = new JLabel();
+
         setLayout(null);
 
         //ustalanie wielkości oraz rozmieszczania przycisków
         userLoginLabel.setBounds(50,100,75,25);
-        userPasswordLabel.setBounds(50,150,75,25);
         login.setBounds(125,200,100,25);
 
         //sprawdzanie czy użytkownik podał wprowadził dane należące do bazy
         login.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String login = userNameField.getText();
-                String password = String.valueOf(userPasswordField.getPassword());
-
-                if(loginInfo.containsKey(login)){
-                    if(loginInfo.get(login).equals(password)){
-                        //ustawia kolor tła na zielony, który nie razi aż tak w oczy
-                        getContentPane().setBackground(new Color(0x00CC66));
-                    }
-                    else  getContentPane().setBackground(new Color(0xCC0000));
-                }
-                else  getContentPane().setBackground(new Color(0xCC0000));
             }
         });
 
@@ -69,22 +54,10 @@ public class MainWindow extends JFrame {
         cancel.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                userNameField.setText("");
-                userPasswordField.setText("");
-                getContentPane().setBackground(null);
             }
         });
-        login.setFocusable(false);
-        cancel.setFocusable(false);
-
-        userNameField.setBounds(125,100,200,25);
-        userPasswordField.setBounds(125,150,200,25);
 
         add(userLoginLabel);
-        add(userPasswordLabel);
-        add(userNameField);
-        add(userPasswordField);
-        add(messageLabel);
         add(login);
         add(cancel);
     }
