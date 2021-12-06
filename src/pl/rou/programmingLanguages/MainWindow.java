5f7ae1b3af85c8b0101a358f27ea6d9e9303854b
Randarola <67556224+Randarola@users.noreply.github.com>
2021-12-06 10:24:17 +0100

    changing buttons's names

--- src/pl/rou/programmingLanguages/MainWindow.java
+++ src/pl/rou/programmingLanguages/MainWindow.java
@@ -1,12 +1,9 @@
 package pl.rou.programmingLanguages;
 //korzystam z fragmentów kodu z zajęć
+
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.InputEvent;
-import java.awt.event.KeyEvent;
-import java.util.HashMap;
 
 public class MainWindow extends JFrame {
     public static void main(String[] args) {
@@ -21,44 +18,48 @@ public class MainWindow extends JFrame {
             }
         });
     }
+
     public MainWindow(String title) throws Exception {
         super(title);
 
         buildFrame();
     }
-    protected void buildFrame(){
 
+    protected void buildFrame() {
+// !!!  dopasuj lepsze parametry, wyskakujące okno jest za małe !!!
         setBounds(100, 100, 450, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JButton login = new JButton("Log in");
+
+        JLabel info = new JLabel("Tu będzie się znajdował komunikat odnośnie działania programu");
+        JButton run = new JButton("Start");
         JButton cancel = new JButton("Cancel");
 
-
-        JLabel userLoginLabel = new JLabel("Login: ");
 
         setLayout(null);
 
         //ustalanie wielkości oraz rozmieszczania przycisków
-        userLoginLabel.setBounds(50,100,75,25);
-        login.setBounds(125,200,100,25);
+        // !!!  dopasuj lepsze parametry, słabe rozmieszczenie przycisków!!!
+        info.setBounds(50, 100, 400, 25);
+        run.setBounds(125, 200, 100, 25);
+        cancel.setBounds(225, 200, 100, 25);
 
         //sprawdzanie czy użytkownik podał wprowadził dane należące do bazy
-        login.addActionListener(new ActionListener() {
+        run.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
             }
         });
 
-        cancel.setBounds(225,200,100,25);
+
         cancel.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
             }
         });
 
-        add(userLoginLabel);
-        add(login);
+        add(info);
+        add(run);
         add(cancel);
     }
 }
